dbe6ac15b32d11fc6e20de19a854db081a0a7d14
Anil <anilkumarreddy778@gamil.com>
2018-07-07 17:44:53 +0530

    5th commit wiht - flotting popup screen

--- app/src/main/java/steedserv/com/eco_agri/homepage.java
+++ app/src/main/java/steedserv/com/eco_agri/homepage.java
@@ -1,11 +1,16 @@
 package steedserv.com.eco_agri;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
@@ -45,7 +50,7 @@ public class homepage extends AppCompatActivity {
 
         GridView simpleGrid;
         int animal[] = {R.drawable.farmer, R.drawable.money, R.drawable.seeds, R.drawable.paddy, R.drawable.cow, R.drawable.bufflo, R.drawable.sheep, R.drawable.milkbottle,
-                R.drawable.tractor, R.drawable.sickle, R.drawable.technol, R.drawable.home, R.drawable.corn, R.drawable.greenfood};
+                R.drawable.tractor, R.drawable.sickle, R.drawable.technol, R.drawable.home, R.drawable.corn, R.drawable.greenfood,R.drawable.greenfood,R.drawable.greenfood,R.drawable.greenfood,R.drawable.greenfood,R.drawable.greenfood};
 
 
         simpleGrid = (GridView) findViewById(R.id.gridview);
@@ -56,6 +61,46 @@ public class homepage extends AppCompatActivity {
         Intent intent = new Intent(homepage.this, homepage.class);
         startActivity(intent);
     }
+
+    public void showpopup(View view) {
+        View view1 = LayoutInflater.from(this).inflate(R.layout.homepopupvalues, null);
+
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setView(view1)
+                .setCancelable(true)
+                .setTitle("Enter Details :")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        dialog.dismiss();
+                    }
+                }) //Set to null. We override the onclick
+                .setNegativeButton(android.R.string.cancel, null)
+                .create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener()
+        {
+            @Override
+            public void onShow(final DialogInterface dialog)
+            {
+                Button b = ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                b.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View view)
+                    {
+
+                    }
+                });
+            }
+        });
+        dialog.show();
+    }
+
+//    this popup screen code
+
+
 }
 
 
