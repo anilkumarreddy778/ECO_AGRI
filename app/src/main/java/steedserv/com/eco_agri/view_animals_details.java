30d1f54e4a3910f28ac2f268e1d9c8dcb6582620
Anil <anilkumarreddy778@gamil.com>
2018-07-25 18:46:31 +0530

    25-07-18 added new activety view animals details and UI development in view animals details

---
+++ app/src/main/java/steedserv/com/eco_agri/view_animals_details.java
@@ -0,0 +1,13 @@
+package steedserv.com.eco_agri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class view_animals_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_animals_details);
+    }
+}
