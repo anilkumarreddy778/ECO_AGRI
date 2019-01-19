5db33d2a81a7dd69ce8ad68935dc0c1567b85383
madhu <kankurtemadhusudhan1993@gmail.com>
2019-01-19 10:45:21 +0530

    fixed.

--- app/src/main/java/steedserv/com/eco_agri/report_Fragment.java
+++ app/src/main/java/steedserv/com/eco_agri/report_Fragment.java
@@ -14,23 +14,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link report_Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link report_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
+
 public class report_Fragment extends Fragment {
+
+    LinearLayout lLayout;
 
     @Override
     public NestedScrollView onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,7 +28,7 @@ public class report_Fragment extends Fragment {
 
 //        LinearLayout lLayout = (LinearLayout) rootView.findViewById(R.id.wagesusersf);
 //     lLayout.addView( rootView );
-        LinearLayout lLayout = (LinearLayout) rootView.findViewById(R.id.wagesusersf);
+        lLayout = (LinearLayout) rootView.findViewById(R.id.wagesusersf);
 
         return (NestedScrollView) rootView;
     }
@@ -49,121 +36,53 @@ public class report_Fragment extends Fragment {
     public void gobacktowagesdetalis(View view)
     {
     }
+
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        View rootView = null;
-        view= getLayoutInflater().inflate( R.layout.user_wages_view, null );
-        LinearLayout lLayout =null;
+
+
+
 
         for (int i = 0; i < 4; i++) {
             if (i == 0) {
-                TextView imageView = view.findViewById( R.id.username );
+                View rootview =  getLayoutInflater().inflate(R.layout.user_wages_view, null,false);
+                TextView imageView = rootview.findViewById( R.id.username );
                 imageView.setText( "Babu" );
-                EditText providerName = view.findViewById( R.id.amount );
+                EditText providerName = rootview.findViewById( R.id.amount );
                 providerName.setText( "500" );
                 // Assigning value to  imageview and textview here
-                lLayout.addView( view );
+                lLayout.addView( rootview );
             }
             if (i == 1) {
-                TextView imageView = view.findViewById( R.id.username );
+                View rootview =  getLayoutInflater().inflate(R.layout.user_wages_view, null,false);
+                TextView imageView = rootview.findViewById( R.id.username );
                 imageView.setText( "Basava" );
-                EditText providerName = view.findViewById( R.id.amount );
+                EditText providerName = rootview.findViewById( R.id.amount );
                 providerName.setText( "400" );
-                lLayout.addView(view);
+                lLayout.addView(rootview);
             }
             if (i == 2) {
-                TextView imageView = view.findViewById( R.id.username );
+                View rootview =  getLayoutInflater().inflate(R.layout.user_wages_view, null,false);
+                TextView imageView = rootview.findViewById( R.id.username );
                 imageView.setText( "Surya" );
-                EditText providerName = view.findViewById( R.id.amount );
+                EditText providerName = rootview.findViewById( R.id.amount );
                 providerName.setText( "600" );
-                lLayout.addView( view );
+                lLayout.addView( rootview );
             }
             if (i == 3) {
-                TextView imageView = view.findViewById( R.id.username );
+                View rootview =  getLayoutInflater().inflate(R.layout.user_wages_view, null,false);
+                TextView imageView = rootview.findViewById( R.id.username );
                 imageView.setText( "Anil" );
-                EditText providerName = view.findViewById( R.id.amount );
+                EditText providerName = rootview.findViewById( R.id.amount );
                 providerName.setText( "300" );
-                lLayout.addView( view );
+                lLayout.addView( rootview );
             }
         }
     }
-
-    public report_Fragment() {
-        // Required empty public constructor
-    }
-
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment report_Fragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static report_Fragment newInstance(String param1, String param2) {
-//        report_Fragment fragment = new report_Fragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_wages, container, false);
-//    }
-//
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener
     {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
 }
